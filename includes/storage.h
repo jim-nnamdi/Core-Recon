893300b884fbe3876ca9c2415346dc166d10cd8a
Jim <jimsamuel50@gmail.com>
2024-12-18 13:42:45 +0100

    create_new_user method onsubmit for auth_handler

--- includes/storage.h
+++ includes/storage.h
@@ -2,5 +2,5 @@
 #include <stdlib.h>
 #include "../storage/sqlite3.h"
 
-int create_storage_db(const char* database_name);
+sqlite3* create_storage_db(const char* database_name);
 int create_new_user(sqlite3* db, const char* username, const char* password, const char* email);
