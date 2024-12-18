0f8b969cab20343a59c9da427fa466fd45630882
Jim <jimsamuel50@gmail.com>
2024-12-18 13:33:11 +0100

    storage insert method on creating user

--- includes/storage.h
+++ includes/storage.h
@@ -1,4 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "../storage/sqlite3.h"
 
 int create_storage_db(const char* database_name);
+int create_new_user(sqlite3* db, const char* username, const char* password, const char* email);
--- storage/storage.c
+++ storage/storage.c
@@ -21,3 +21,19 @@ int create_storage_db(const char* database_name){
     sqlite3_close(&db);
     return EXIT_SUCCESS;
 }
+
+int create_new_user(sqlite3* db, const char* username, const char* password, const char* email){
+    const char* add_user_query = "insert into users(username, password, email_address) values(?,?,?)";
+    sqlite3_stmt* stmt; int result_stmt;
+    result_stmt = sqlite3_prepare_v2(&db, add_user_query, -1, &stmt, NULL);
+    if (result_stmt != SQLITE_OK) fprintf(stderr, _sqlite_exec_err); sqlite3_errmsg(&db);
+
+    sqlite3_bind_text(stmt, 1, username, -1, SQLITE_STATIC);
+    sqlite3_bind_text(stmt, 2, password, -1, SQLITE_STATIC);
+    sqlite3_bind_text(stmt, 3, email, -1, SQLITE_STATIC);
+
+    result_stmt = sqlite3_step(&stmt);
+    if (result_stmt != SQLITE_DONE) printf(_sqlite_exec_err);
+    sqlite3_finalize(stmt);
+    return result_stmt == SQLITE_DONE ? SQLITE_OK : result_stmt;
+}
