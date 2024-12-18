abe3c83683a2a337484e12f4d4f1426336d813ec
Jim <jimsamuel50@gmail.com>
2024-12-18 13:16:52 +0100

    use sqlite3 macros to handle error checks

--- storage/storage.c
+++ storage/storage.c
@@ -6,13 +6,13 @@
 int create_storage_db(const char* database_name){
     sqlite3 *db;
     int create_storage = sqlite3_open(database_name, &db);
-    if (!create_storage) {
+    if (create_storage != SQLITE_OK) {
         fprintf(stderr, _sqlite_open_err); 
         sqlite3_close(&db);
         return EXIT_FAILURE;
     }
     create_storage = sqlite3_exec(&db, _create_users_table, 0, 0, &_generic_err_msg);
-    if (!create_storage) {
+    if (create_storage != SQLITE_OK) {
         fprintf(stderr, _sqlite_exec_err); 
         sqlite3_free(_generic_err_msg);
         sqlite3_close(&db);
